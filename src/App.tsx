6c22f7eebc183c39a41f0a718c53396c2bd3a680
Katherina Kaliaha <mellinda077@mail.ru>
2023-04-30 12:53:00 +0300

    feat: add theme in MantineProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
-import './App.css';
+import { MantineProvider } from '@mantine/core';
+
+import { AppRouter } from './components/Router';
+import { CustomFonts } from './CustomFonts';
+
+export const links = [
+  { link: '/', label: 'Поиск Вакансий' },
+  { link: '/fav', label: 'Избранное' },
+];
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <CustomFonts />
+      <MantineProvider
+        theme={{
+          globalStyles: () => ({
+            '*, *::before, *::after': {
+              boxSizing: 'border-box',
+            },
+          }),
+          colors: {
+            blueVariants: ['#5E96FC', '#92C1FF', '#3B7CD3', '#DEECFF'],
+            grayVariants: ['#ACADB9', '#D5D6DC', '#7B7C88'],
+            whiteVariants: ['#F7F7F8', '#FFFFFF', '#EAEBED'],
+            blackVariants: ['#232134'],
+          },
+          fontSizes: {
+            xs: '0.875rem',
+            sm: '1rem',
+            md: '1.25rem',
+            lg: '1.5rem',
+            xl: '1.75rem',
+          },
+        }}
+        withNormalizeCSS
+        withGlobalStyles
+      >
+        <AppRouter />
+      </MantineProvider>
+    </>
+  );
 }
 
 export default App;
