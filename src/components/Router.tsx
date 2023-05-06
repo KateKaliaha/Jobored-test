c135cfab6bf7a581fa60e9366eb98988dbb50664
Katherina Kaliaha <mellinda077@mail.ru>
2023-05-06 19:30:39 +0300

    refactor: code review

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,8 +1,9 @@
 import { AppShell, createStyles } from '@mantine/core';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { links } from '../App';
-import { MainPage } from '../pages/main';
+import { Favorites as FavoritesPage } from '../pages/Favorites';
+import { Main as MainPage } from '../pages/Main';
+import { Vacancy as VacancyPage } from '../pages/Vacancy';
 import { HeaderResponsive } from './Header';
 
 const useStyles = createStyles((theme) => ({
@@ -15,18 +16,19 @@ const useStyles = createStyles((theme) => ({
 
 export const AppRouter = () => {
   const { classes } = useStyles();
+
   return (
     <Router>
       <AppShell
         classNames={{
           main: classes.main,
         }}
-        header={<HeaderResponsive links={links} />}
+        header={<HeaderResponsive />}
       >
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/:id" element={<h1>item page</h1>} />
-          <Route path="/fav" element={<h1>fav</h1>} />
+          <Route path="/:id" element={<VacancyPage />} />
+          <Route path="/fav" element={<FavoritesPage />} />
         </Routes>
       </AppShell>
     </Router>
