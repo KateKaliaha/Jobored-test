dedf6d39bb4f2773411e23ae1343727ba476a2e4
Katherina Kaliaha <mellinda077@mail.ru>
2023-05-06 23:33:08 +0300

    refactor: code review

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
 import { MantineProvider } from '@mantine/core';
+import { useEffect } from 'react';
 
 import { AppRouter } from './components/Router';
 import { CustomFonts } from './CustomFonts';
-
-export const links = [
-  { link: '/', label: 'Поиск Вакансий' },
-  { link: '/fav', label: 'Избранное' },
-];
+import { useAppDispatch, useAppSelector } from './store';
+import { fetchAuth } from './store/authSlice';
+import { fetchIndustriesCatalog } from './store/industriesSlice';
+import { fetchVacanciesCatalog } from './store/vacanciesSlice';
 
 function App() {
+  const dispatch = useAppDispatch();
+  const { auth } = useAppSelector((state) => state.auth);
+  const { payment_from, catalogueKey, payment_to, page, keyword } = useAppSelector(
+    (state) => state.mainPage,
+  );
+  const { token } = useAppSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!auth) {
+      dispatch(fetchAuth());
+    }
+  }, []);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(fetchIndustriesCatalog({ token }));
+      dispatch(
+        fetchVacanciesCatalog({
+          token,
+          page: page - 1,
+          keyword,
+          payment_from,
+          payment_to,
+          catalogueKey,
+        }),
+      );
+    }
+  }, [token]);
+
   return (
     <>
       <CustomFonts />
@@ -23,7 +52,7 @@ function App() {
             blueVariants: ['#5E96FC', '#92C1FF', '#3B7CD3', '#DEECFF'],
             grayVariants: ['#ACADB9', '#D5D6DC', '#7B7C88'],
             whiteVariants: ['#F7F7F8', '#FFFFFF', '#EAEBED'],
-            blackVariants: ['#232134'],
+            blackVariants: ['#232134', '#343A40', '#202021'],
           },
           fontSizes: {
             xs: '0.875rem',
@@ -43,3 +72,7 @@ function App() {
 }
 
 export default App;
+
+addEventListener('unload', () => {
+  localStorage.delete('auth');
+});
